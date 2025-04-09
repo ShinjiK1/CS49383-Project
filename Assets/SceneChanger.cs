e6f6e64816e7e818edf6d3f9e23daf097111f838
ShinjiK <shinjojobojo@gmail.com>
2025-04-09 01:29:06 -0400

    Added temp scene change functionality

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public InputActionReference changeSceneButton;
+    private int numScenes = 2;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        changeSceneButton.action.Enable();
+        changeSceneButton.action.performed += NextScene;
+    }
+
+    private void NextScene(InputAction.CallbackContext context)
+    {
+        SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % numScenes);
+    }
+}
